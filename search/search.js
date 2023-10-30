72ba857950ade91bcd46e8d9970a20cfec5565db
samuellouf <SamuelLoufCoder@gmail.com>
2023-10-30 19:19:28 +0100

    Search and My Projects

---
+++ search/search.js
@@ -0,0 +1,23 @@
+async function loadPages(){
+  var api = '';
+}
+
+function refreshSearchResults() {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('searchInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("searchItems");
+  li = ul.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("div")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
