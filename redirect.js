555cc5853b5818ffec5a08f60791d0445011057e
samuellouf <SamuelLoufCoder@gmail.com>
2024-09-11 14:21:37 +0200

    Added 404.html redirection

---
+++ redirect.js
@@ -0,0 +1,30 @@
+function tryto(func){
+  return new Promise(async (resolve) => {
+    try{
+      var r = await func();
+      resolve(r);
+    } catch (e){
+      resolve(e);
+    }
+  })
+}
+
+document.addEventListener('DOMContentLoaded', async function (event) {
+  var func = async () => {
+    var redirections = (await fetch('https://samuellouf.github.io/api/samuellouf-website/v1/redirections.json')).then((r) => r.json());
+    const isURLCompatible = (origin) => {
+      return window.location.href.startsWith(origin);
+    }
+
+    const getNewURL = (origin, redirection) => {
+      return redirection + window.location.href.replace(origin, '') + (window.location.href.replace(origin, '')[1] != '/' ? '/' : '');
+    }
+
+    for (var url of redirections){
+      if (isURLCompatible(url.origin)){
+        window.location.href = getNewURL(url.origin, url.redirection);
+      }
+    }
+  }
+  tryto(func);
+})
